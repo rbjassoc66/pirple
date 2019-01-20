532071e23c0dcdf1eeb1eafe2bf0260f0903fc2b
rbjassoc66 <bobj@rbjassoc.com>
2019-01-19 20:21:40 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+'''For this assignment I have selected  Niel Diamond
+    hope you enjoy! '''
+
+
+################## Variables ########################
+
+artist = 'Niel Diamond'
+title = ' Red Red Wine'
+release_year = 1967
+genre = 'pop'
+duration = '2:42'
+
+print(' The artist I chose for this assignment is %s, and the title is %s,\n it was released in %d and is considered %s genre. The duration of the song is %s' %(artist,title,release_year,genre,duration))
